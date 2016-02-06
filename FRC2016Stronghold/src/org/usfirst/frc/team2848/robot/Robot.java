9031da89d5c25a4d1211649cf51ef4f8d3a01bf2
roflmaoqwertyqaz <tkdduo@gmail.com>
2016-02-06 14:27:15 -0600

    drivetrain

--- FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/Robot.java
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/Robot.java
@@ -1,44 +1,77 @@
 
 package org.usfirst.frc.team2848.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+//import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+//import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-	
+    public static Joystick xbox1;
+    public static Talon bottomLeft;
+    public static Talon topRight;
+    public static Solenoid shifter1;
+    public static Compressor compressor1;
+    public static PowerDistributionPanel pdp;
+    /*
+    public static PID pidleft;
+    public static PID pidright;
+    double outputbottom;
+    double outputtop;
+    double gearratio; 
+    public static double targetbottom;
+    public static double targettop;
+    public static Encoder encoderleft;
+    public static Encoder encoderright;
+    */
 	RobotDrive drivetrain;
     public void robotInit() {
+    	bottomLeft = new Talon(0);
+    	topRight = new Talon(1);
     	drivetrain = new RobotDrive(0,1);
+    	shifter1 = new Solenoid(0,1);
+    	compressor1 = new Compressor();
+    	pdp = new PowerDistributionPanel();
+    	//gearratio = (2/1); //get real gear ratio from mechanical subteam when they knows it
+    	/*
+    	  targetbottom = 0;
+          targettop = 0;
+          encoderleft = new Encoder(0, 1, false, EncodingType.k4X);
+          encoderright = new Encoder(2, 3, true, EncodingType.k4X);
+         // encoderleft.setReverseDirection(true); if one is negative reverse it using this
+          encoderleft.setDistancePerPulse((1/1024.0)*gearratio*60);
+          encoderright.setDistancePerPulse((1/1024.0)*gearratio*60);
+          
+          pidleft = new PID(0, 0, 0, 0, encoderleft.getRate());
+          pidright = new PID(0, 0, 0, 0, encoderright.getRate());
+          
+          pidleft.setBounds(-1, 1);
+          pidright.setBounds(-1, 1);
+          pidleft.setITermBounds(-1, 1);
+          pidright.setITermBounds(-1, 1);
+          */
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
+  
     public void autonomousPeriodic() {
 
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
+ 
     public void teleopPeriodic() {
-        
+    	compressor1.setClosedLoopControl(true);
+    	SparkyPnuematics.pneumaticToggle();
+        SparkyDriveHelper.arcadeDrive(xbox1, drivetrain);
+        System.out.println(pdp.getCurrent(12) + " "+ pdp.getCurrent(13) + " "+ pdp.getCurrent(14) + " " + pdp.getCurrent(15));
+     
     }
     
-    /**
-     * This function is called periodically during test mode
-     */
+    
     public void testPeriodic() {
     
     }
