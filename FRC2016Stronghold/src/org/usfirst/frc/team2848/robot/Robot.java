da3e8f299f740ec1d6a7d5cc9da6f0bb7284ca16
roflmaoqwertyqaz <tkdduo@gmail.com>
2016-02-08 17:05:04 -0600

    initialized motors and other things

--- FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/Robot.java
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/Robot.java
@@ -2,78 +2,41 @@
 package org.usfirst.frc.team2848.robot;
 
 import edu.wpi.first.wpilibj.Compressor;
-//import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
-//import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 
 public class Robot extends IterativeRobot {
-    public static Joystick xbox1;
-    public static Talon bottomLeft;
-    public static Talon topRight;
-    public static Solenoid shifter1;
-    public static Compressor compressor1;
-    public static PowerDistributionPanel pdp;
-    /*
-    public static PID pidleft;
-    public static PID pidright;
-    double outputbottom;
-    double outputtop;
-    double gearratio; 
-    public static double targetbottom;
-    public static double targettop;
-    public static Encoder encoderleft;
-    public static Encoder encoderright;
-    */
-	RobotDrive drivetrain;
+public static Talon motor1;
+public static Talon motor2;
+public static Joystick xbox1;
+public static Solenoid shifter1;
+public static RobotDrive drivetrain;
+public static Compressor compressor;
     public void robotInit() {
-    	bottomLeft = new Talon(0);
-    	topRight = new Talon(1);
-    	drivetrain = new RobotDrive(new Talon(0),new Talon(1));
-    	shifter1 = new Solenoid(5,0);
-    	compressor1 = new Compressor(6);
-    	pdp = new PowerDistributionPanel();
-    	//gearratio = (2/1); //get real gear ratio from mechanical subteam when they knows it
-    	/*
-    	  targetbottom = 0;
-          targettop = 0;
-          encoderleft = new Encoder(0, 1, false, EncodingType.k4X);
-          encoderright = new Encoder(2, 3, true, EncodingType.k4X);
-         // encoderleft.setReverseDirection(true); if one is negative reverse it using this
-          encoderleft.setDistancePerPulse((1/1024.0)*gearratio*60);
-          encoderright.setDistancePerPulse((1/1024.0)*gearratio*60);
-          
-          pidleft = new PID(0, 0, 0, 0, encoderleft.getRate());
-          pidright = new PID(0, 0, 0, 0, encoderright.getRate());
-          
-          pidleft.setBounds(-1, 1);
-          pidright.setBounds(-1, 1);
-          pidleft.setITermBounds(-1, 1);
-          pidright.setITermBounds(-1, 1);
-          */
+    	motor1 = new Talon(0);
+    	motor2 = new Talon(1);
+    	drivetrain = new RobotDrive(new Talon(0), new Talon(1));
+    	xbox1 = new Joystick(0);
+    	shifter1 = new Solenoid(0);
+    	compressor = new Compressor(6);
     }
 
-  
     public void autonomousPeriodic() {
 
     }
 
- 
     public void teleopPeriodic() {
-    	compressor1.setClosedLoopControl(true);
-    	SparkyPnuematics.pneumaticToggle();
+    	compressor.setClosedLoopControl(true);
         SparkyDriveHelper.arcadeDrive(xbox1, drivetrain);
-        System.out.println(pdp.getCurrent(12) + " "+ pdp.getCurrent(13) + " "+ pdp.getCurrent(14) + " " + pdp.getCurrent(15));
-     
+        SparkyMotorController.controlMotors();
+        SparkyPnuematics.pneumaticToggle();
     }
     
-    
     public void testPeriodic() {
-    	
+    	//hello
     }
     
 }
