6d2644369f02ce4804f1e1b3c73ae729fed4ff45
10rm <ryanmurray_10@yahoo.com>
2016-02-11 17:41:58 -0600

    changed structure

--- FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/Shooter.java
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/Shooter.java
@@ -3,45 +3,25 @@ package org.usfirst.frc.team2848.robot.subsystems;
 import org.usfirst.frc.team2848.robot.Robot;
 
 public class Shooter {
-	private static boolean shifterstate= false;
-	
-	private static boolean firing = false;
 	private static double start;
 	
-	public static void fire(){
-		if (firing){
-			if (Robot.xbox1.getRawButton(2) && !shifterstate) {
-				shifterstate = true;
+	public static void firingRoutine(double speed){
+		if (Robot.xbox1.getRawButton(1)){
+			if (Robot.xbox1.getRawButton(2) && !Robot.shootertrigger.get()) {
+				Robot.shootertrigger.set(true);
 				start = System.currentTimeMillis();
-				SparkyIntakeBar.loaded = false;
 			}
-			if (shifterstate && (System.currentTimeMillis() > start + 250)) {
-				firing = false;
-				shifterstate = false;
+			if (Robot.shootertrigger.get() && (System.currentTimeMillis() > start + 250)) {
+				Robot.shootertrigger.set(false);
 			}
-			Robot.shootertrigger.set(shifterstate);
-		}
-	}
-	public static void isFiring(){
-		if (Robot.xbox1.getRawButton(1) && SparkyIntakeBar.loaded){
-			firing = true;
-		}
-	}
-	public static void spinUp(double speed){
-		if (firing){
 			Robot.shooterpidleft.setTarget(speed);
 			Robot.shooterpidright.setTarget(speed);
 		}
-		else{
+		else {
 			Robot.shooterpidleft.setTarget(0);
 			Robot.shooterpidright.setTarget(0);
 		}
 		Robot.shooterleftmotor.set(Robot.shooterpidleft.compute(Robot.shooterleftenc.getRate()));
 		Robot.shooterrightmotor.set(Robot.shooterpidright.compute(Robot.shooterrightenc.getRate()));
-	}
-	public static void firingRoutine(){
-		isFiring();
-        fire();
-        spinUp(1500);
 	}
 }
