3ad0acc356646ad0edfd93c15778941490cee0c4
roflmaoqwertyqaz <tkdduo@gmail.com>
2016-02-15 16:18:04 -0600

    Added control to intake arm and jacks gear box

--- FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/Robot.java
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/Robot.java
@@ -1,13 +1,13 @@
 
 package org.usfirst.frc.team2848.robot;
 import org.usfirst.frc.team2848.robot.subsystems.DriveHelper;
-import org.usfirst.frc.team2848.robot.subsystems.Pnuematics;
 import org.usfirst.frc.team2848.robot.subsystems.autoShifter;
+import org.usfirst.frc.team2848.robot.subsystems.motorController;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
@@ -16,18 +16,27 @@ import edu.wpi.first.wpilibj.Timer;
 public class Robot extends IterativeRobot {
 public static Joystick xbox1;
 public static Solenoid shifter1;
+public static DoubleSolenoid shifterArm;
+public static DoubleSolenoid raiseArm;
+public static DoubleSolenoid extendArm;
 public static RobotDrive drivetrain;
 public static Compressor compressor;
 public static Talon bottomleft;
 public static Talon bottomright;
+public static Talon intakeMotor;
+public static Talon armMotor;
 
-    public void robotInit() {
-    	
+    public void robotInit() {  	
+    	armMotor = new Talon(2);
     	bottomleft = new Talon(0);
     	bottomright = new Talon(1);
+    	intakeMotor= new Talon(3);
     	drivetrain = new RobotDrive(bottomleft, bottomright);
     	xbox1 = new Joystick(0);
     	shifter1 = new Solenoid(5,0);
+    	shifterArm = new DoubleSolenoid(5,1,2);
+    	raiseArm = new DoubleSolenoid(5,3,4);
+    	extendArm = new DoubleSolenoid(5,5,6);
     	compressor = new Compressor(5);
     }
 
@@ -37,10 +46,15 @@ public static Talon bottomright;
 
     public void teleopPeriodic() {
     	compressor.setClosedLoopControl(true);
-        DriveHelper.arcadeDrive(xbox1, drivetrain);
-        Pnuematics.pneumaticToggle();
         autoShifter.downShift();
-        Timer.delay(.01);
+//       motorController.controlMotors();
+       DriveHelper.arcadeDrive(xbox1, drivetrain); 
+    	//implement code so that the motor cant go backwards when on its climbing gear
+    	if (xbox1.getRawButton(3)) armMotor.set(.2);
+    	else if (xbox1.getRawButton(4)) armMotor.set(-.2);
+    	else armMotor.set(0);
+    	
+        
     }
     
     public void testPeriodic() {
--- FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/autoShifter.java
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/autoShifter.java
@@ -1,28 +1,110 @@
 package org.usfirst.frc.team2848.robot.subsystems;
 import org.usfirst.frc.team2848.robot.Robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class autoShifter{
 	public static PowerDistributionPanel pdp;
-	private static boolean  state = false;
-	public static void downShift()
-	{
+	private static long start;
+	private static boolean danger;
+	public static boolean state = true;
+	public static boolean stateArm = true;
+	public static boolean raiseIntake = false;
+	public static boolean extendIntake = false;
+	public static int SHIFTTIME = 500;
+	public static void downShift() {	
+		if (Robot.xbox1.getRawAxis(3) > 0.75) { //for drive train
+				
+			state = true;
+		}
+		if (Robot.xbox1.getRawButton(6)){ //for drive train 
+				
+			state = false;
+		}
+		if(Robot.xbox1.getRawButton(1)) // a = 1 b = 2 //for gear box 
+		{
+			stateArm = true;
+		}
+		if(Robot.xbox1.getRawButton(2)) // a = 1 b = 2 //for gear box
+		{
+			stateArm = false;
+		}
+		if(Robot.xbox1.getRawButton(5)) // a = 1 b = 2 //for raise intake arm
+		{
+			raiseIntake = true;
+		}
+		if(Robot.xbox1.getRawAxis(2) > 0.75) // a = 1 b = 2 //Extend intake bar
+		{
+			extendIntake = true;
+		}
+		
+		
 		pdp = new PowerDistributionPanel();
-		long start = System.nanoTime();
-		long end = System.nanoTime() + 500;
 		double voltage = pdp.getVoltage();
-			if(voltage < 9.0 && start < end)
+	
+		if(!Robot.shifter1.get())
+		{
+			if(voltage <= 9.0) //&& start < end)
 			{
-				state = true;
-				voltage = pdp.getVoltage();
+				if(!danger) {
+					danger = true;
+					start = System.currentTimeMillis();
+				}
+				if(danger)
+				{
+					if(voltage <=8.0)
+					{
+						state = true; //low gear instant
+						danger = false;
+						System.out.println("below 8 V");
+					}
+				}
+				if(danger) {
+					if(System.currentTimeMillis()-start > SHIFTTIME) {
+						state = true;
+						danger = false;
+					}
+				}
 			}
-			if(state)
+			else {
+				danger = false;
+			}
+			
+	}
+	else {
+		danger = false;
+	}
+		
+		if(!raiseIntake)
+		{
+			//lower intake bar
+			Robot.raiseArm.set(Value.kReverse);
+		}
+		else
+		{
+			//raise intake bar
+			Robot.raiseArm.set(Value.kForward);
+		}
+		
+			if(!extendIntake)
 			{
-				Robot.shifter1.set(false);
-				System.out.println( "Down SHIFTED!!!!!!!!!!!!" +"\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n"+ "Down SHIFTED!!!!!!!!!!!!" + "\n"+"Down SHIFTED!!!!!!!!!!!!" + "\n" +"Down SHIFTED!!!!!!!!!!!!" + "\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n"+"Down SHIFTED!!!!!!!!!!!!" + "\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n"+"Down SHIFTED!!!!!!!!!!!!" + "\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n"+"Down SHIFTED!!!!!!!!!!!!" + "\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n"+ "Down SHIFTED!!!!!!!!!!!!" +"\n");
+				//suck in intake bar
+				Robot.extendArm.set(Value.kReverse);
 			}
-		state = false;
+			else
+			{
+				//extend intake bar
+				Robot.extendArm.set(Value.kForward);
+			}
+		
+		//double solenoid shifter
+			
+		Robot.shifterArm.set(stateArm ? Value.kForward : Value.kReverse);
+		Robot.shifter1.set(state);
+			System.out.println(stateArm);
+			
+		
 	}
 	
 }
