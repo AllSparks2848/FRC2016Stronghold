d7082b6e5f696a579cff1c7558e28a5f31dba115
10rm <ryanmurray_10@yahoo.com>
2016-02-16 19:42:04 -0600

    fixed arm

--- FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/Arm.java
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/Arm.java
@@ -6,13 +6,12 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public class Arm {
 	public static void armRoutine(){
-		if (Math.abs(Definitions.xbox2.getRawAxis(0)) <= 0.25) {
+		if (Math.abs(Definitions.xbox2.getRawAxis(1)) <= 0.25) {
 			Definitions.ptoshifter.set(Value.kReverse);
 			Definitions.armbrake.set(Value.kForward);
-			Definitions.ptomotor.set(Definitions.xbox2.getRawAxis(0));
+			Definitions.ptomotor.set(Definitions.xbox2.getRawAxis(1));
 		}
 		else {
-			Definitions.ptoshifter.set(Value.kForward);
 			Definitions.armbrake.set(Value.kReverse);
 			Definitions.ptomotor.set(0);
 		}
