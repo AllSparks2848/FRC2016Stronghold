189e8c7a898f490a1eb4046e7976590a2bb0790a
roflmaoqwertyqaz <tkdduo@gmail.com>
2016-02-19 17:40:41 -0600

    added centerTurret Method

---
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/Turret.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team2848.robot.subsystems;
+
+import org.usfirst.frc.team2848.robot.Definitions;
+
+public class Turret {
+	public static void turretRoutine() {
+		if(Definitions.lowerarmlimit.get())
+		{
+			Definitions.turret.set(Definitions.xbox2.getRawAxis(4)*0.4);
+		}
+		else
+		{
+			Definitions.turret.set(0);
+		}
+	}
+	public static void centerTurret()
+	{
+		double directionToMove = 0;
+		if(Definitions.turretenc.get() > 0)
+		{
+			directionToMove = -.5;
+		}
+		else if(Definitions.turretenc.get() < 0)
+		{
+			directionToMove = .5;
+		}
+		else if(Definitions.turretenc.get() <= 150 && Definitions.turretenc.get() >= -150)
+		{
+			directionToMove = 0;
+		}
+		System.out.println(Definitions.turretenc.get());
+		Definitions.turret.set(directionToMove);
+	}
+}
