8bff12cabf8397bf9c06f7f2a73eb8568cf8e2a4
10rm <ryanmurray_10@yahoo.com>
2016-03-08 17:40:09 -0600

    dashboard

--- FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/DriveShifter.java
+++ FRC2016Stronghold/src/org/usfirst/frc/team2848/robot/subsystems/DriveShifter.java
@@ -4,29 +4,27 @@ import org.usfirst.frc.team2848.robot.Definitions;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class DriveShifter {
 	private static long start;
 	private static boolean danger;
-	private static boolean state = false;
+	private static boolean high = false;
 	private static int SHIFTTIME = 500;
 	
 	public static void checkGearShift() {	
-		if (Definitions.xbox1.getRawAxis(3) > 0.75) { //for drive train
-				
-			state = true;
+		if (Definitions.xbox1.getRawAxis(3) > 0.75) {
+			high = false;
 		}
-		if (Definitions.xbox1.getRawButton(6)){ //for drive train 
-				
-			state = false;
+		if (Definitions.xbox1.getRawButton(6)){
+			high = true;
 		}
-		
 		double voltage = Definitions.pdp.getVoltage();
 	
 		if(Definitions.driveshifter.get())
 		{
-			if(voltage <= 9.0) //&& start < end)
+			if(voltage <= 9.0)
 			{
 				if(!danger) {
 					danger = true;
@@ -36,14 +34,14 @@ public class DriveShifter {
 				{
 					if(voltage <=8.0)
 					{
-						state = true; //low gear instant
+						high = false;
 						danger = false;
 						System.out.println("below 8 V");
 					}
 				}
 				if(danger) {
 					if(System.currentTimeMillis()-start > SHIFTTIME) {
-						state = true;
+						high = false;
 						danger = false;
 					}
 				}
@@ -51,13 +49,11 @@ public class DriveShifter {
 			else {
 				danger = false;
 			}
-			
 	}
 	else {
 		danger = false;
 	}
-			
-		Definitions.driveshifter.set(!state);
+		Definitions.driveshifter.set(high);
+		SmartDashboard.putString("Drive Gear", high ? "high" : "low");
 	}
-	
 }
